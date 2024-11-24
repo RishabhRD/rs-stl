798971d2d46e3d42b93de2e87a5deed71cf09325
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-11-24 17:52:24 +0530

    rename to ranges

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2024 Rishabh Dwivedi (rishabhdwivedi17@gmail.com)
 
-pub mod range;
+pub mod ranges;
 
-pub use range::*;
+pub use ranges::*;
 
 pub fn add(a: u64, b: u64) -> u64 {
     a + b
