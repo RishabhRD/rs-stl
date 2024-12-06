3859f70a30668413fae98607e1b7739f56faaf3b
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-12-06 14:05:51 +0530

    param to be named as R

--- src/algo/find.rs
+++ src/algo/find.rs
@@ -9,15 +9,15 @@ use crate::InputRange;
 //   - Returns position of first element in [start, end) of rng satisfying pred.
 //   - Returns end if no such element exists.
 //   - Complexity: O(n) where n is number if elements in [start, end).
-pub fn find_if<T, F>(
-    rng: &T,
-    mut start: T::Position,
-    end: T::Position,
+pub fn find_if<R, F>(
+    rng: &R,
+    mut start: R::Position,
+    end: R::Position,
     pred: F,
-) -> T::Position
+) -> R::Position
 where
-    T: InputRange,
-    F: Fn(&T::Element) -> bool,
+    R: InputRange,
+    F: Fn(&R::Element) -> bool,
 {
     while start != end {
         if pred(rng.at(&start)) {
