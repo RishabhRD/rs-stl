2c21f090da0a81dfe196a0ef62a28a767fc531e8
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-12-07 16:00:26 +0530

    wrapped tests under cfg(test)

--- tests/oftest.rs
+++ tests/oftest.rs
@@ -1,132 +1,144 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2024 Rishabh Dwivedi (rishabhdwivedi17@gmail.com)
 
-use rng::infix::*;
-use stl::*;
+#[cfg(test)]
+pub mod tests {
+    use rng::infix::*;
+    use stl::*;
 
-#[test]
-fn all_of_for_true() {
-    let vecp = vec![2, 3, 5];
-    let vec = vec![1, 3, 5];
-    let p1 = algo::all_of(&vecp, vecp.after(vecp.start()), vecp.end(), |x| {
-        x % 2 == 1
-    });
-    let p2 = rng::all_of(&vec, |x| x % 2 == 1);
-    let p3 = vec.all_of(|x| x % 2 == 1);
+    #[test]
+    fn all_of_for_true() {
+        let vecp = vec![2, 3, 5];
+        let vec = vec![1, 3, 5];
+        let p1 =
+            algo::all_of(&vecp, vecp.after(vecp.start()), vecp.end(), |x| {
+                x % 2 == 1
+            });
+        let p2 = rng::all_of(&vec, |x| x % 2 == 1);
+        let p3 = vec.all_of(|x| x % 2 == 1);
 
-    assert_eq!(p1, true);
-    assert_eq!(p2, true);
-    assert_eq!(p3, true);
-}
+        assert!(p1);
+        assert!(p2);
+        assert!(p3);
+    }
 
-#[test]
-fn all_of_for_false() {
-    let vecp = vec![2, 4, 5];
-    let vec = vec![1, 4, 5];
-    let p1 = algo::all_of(&vecp, vecp.after(vecp.start()), vecp.end(), |x| {
-        x % 2 == 1
-    });
-    let p2 = rng::all_of(&vec, |x| x % 2 == 1);
-    let p3 = vec.all_of(|x| x % 2 == 1);
+    #[test]
+    fn all_of_for_false() {
+        let vecp = vec![2, 4, 5];
+        let vec = vec![1, 4, 5];
+        let p1 =
+            algo::all_of(&vecp, vecp.after(vecp.start()), vecp.end(), |x| {
+                x % 2 == 1
+            });
+        let p2 = rng::all_of(&vec, |x| x % 2 == 1);
+        let p3 = vec.all_of(|x| x % 2 == 1);
 
-    assert_eq!(p1, false);
-    assert_eq!(p2, false);
-    assert_eq!(p3, false);
-}
+        assert!(!p1);
+        assert!(!p2);
+        assert!(!p3);
+    }
 
-#[test]
-fn all_of_for_empty() {
-    let vecp = vec![2, 4, 5];
-    let vec = vec![];
-    let p1 = algo::all_of(&vecp, vecp.start(), vecp.start(), |x| x % 2 == 1);
-    let p2 = rng::all_of(&vec, |x| x % 2 == 1);
-    let p3 = vec.all_of(|x| x % 2 == 1);
+    #[test]
+    fn all_of_for_empty() {
+        let vecp = vec![2, 4, 5];
+        let vec = vec![];
+        let p1 =
+            algo::all_of(&vecp, vecp.start(), vecp.start(), |x| x % 2 == 1);
+        let p2 = rng::all_of(&vec, |x| x % 2 == 1);
+        let p3 = vec.all_of(|x| x % 2 == 1);
 
-    assert_eq!(p1, true);
-    assert_eq!(p2, true);
-    assert_eq!(p3, true);
-}
+        assert!(p1);
+        assert!(p2);
+        assert!(p3);
+    }
 
-#[test]
-fn any_of_for_true() {
-    let vecp = vec![2, 3, 4];
-    let vec = vec![1, 3, 4];
-    let p1 = algo::any_of(&vecp, vecp.after(vecp.start()), vecp.end(), |x| {
-        x % 2 == 1
-    });
-    let p2 = rng::any_of(&vec, |x| x % 2 == 1);
-    let p3 = vec.any_of(|x| x % 2 == 1);
+    #[test]
+    fn any_of_for_true() {
+        let vecp = vec![2, 3, 4];
+        let vec = vec![1, 3, 4];
+        let p1 =
+            algo::any_of(&vecp, vecp.after(vecp.start()), vecp.end(), |x| {
+                x % 2 == 1
+            });
+        let p2 = rng::any_of(&vec, |x| x % 2 == 1);
+        let p3 = vec.any_of(|x| x % 2 == 1);
 
-    assert_eq!(p1, true);
-    assert_eq!(p2, true);
-    assert_eq!(p3, true);
-}
+        assert!(p1);
+        assert!(p2);
+        assert!(p3);
+    }
 
-#[test]
-fn any_of_for_false() {
-    let vec = vec![2, 4, 6];
-    let p1 =
-        algo::any_of(&vec, vec.after(vec.start()), vec.end(), |x| x % 2 == 1);
-    let p2 = rng::any_of(&vec, |x| x % 2 == 1);
-    let p3 = vec.any_of(|x| x % 2 == 1);
+    #[test]
+    fn any_of_for_false() {
+        let vec = vec![2, 4, 6];
+        let p1 = algo::any_of(&vec, vec.after(vec.start()), vec.end(), |x| {
+            x % 2 == 1
+        });
+        let p2 = rng::any_of(&vec, |x| x % 2 == 1);
+        let p3 = vec.any_of(|x| x % 2 == 1);
 
-    assert_eq!(p1, false);
-    assert_eq!(p2, false);
-    assert_eq!(p3, false);
-}
+        assert!(!p1);
+        assert!(!p2);
+        assert!(!p3);
+    }
 
-#[test]
-fn any_of_for_empty() {
-    let vecp = vec![2, 4, 6];
-    let vec = vec![];
-    let p1 = algo::any_of(&vecp, vecp.start(), vecp.start(), |x| x % 2 == 1);
-    let p2 = rng::any_of(&vec, |x| x % 2 == 1);
-    let p3 = vec.any_of(|x| x % 2 == 1);
+    #[test]
+    fn any_of_for_empty() {
+        let vecp = vec![2, 4, 6];
+        let vec = vec![];
+        let p1 =
+            algo::any_of(&vecp, vecp.start(), vecp.start(), |x| x % 2 == 1);
+        let p2 = rng::any_of(&vec, |x| x % 2 == 1);
+        let p3 = vec.any_of(|x| x % 2 == 1);
 
-    assert_eq!(p1, false);
-    assert_eq!(p2, false);
-    assert_eq!(p3, false);
-}
+        assert!(!p1);
+        assert!(!p2);
+        assert!(!p3);
+    }
 
-#[test]
-fn none_of_for_true() {
-    let vecp = vec![1, 4, 6];
-    let vec = vec![2, 4, 6];
-    let p1 = algo::none_of(&vecp, vecp.after(vecp.start()), vecp.end(), |x| {
-        x % 2 == 1
-    });
-    let p2 = rng::none_of(&vec, |x| x % 2 == 1);
-    let p3 = vec.none_of(|x| x % 2 == 1);
+    #[test]
+    fn none_of_for_true() {
+        let vecp = vec![1, 4, 6];
+        let vec = vec![2, 4, 6];
+        let p1 =
+            algo::none_of(&vecp, vecp.after(vecp.start()), vecp.end(), |x| {
+                x % 2 == 1
+            });
+        let p2 = rng::none_of(&vec, |x| x % 2 == 1);
+        let p3 = vec.none_of(|x| x % 2 == 1);
 
-    assert_eq!(p1, true);
-    assert_eq!(p2, true);
-    assert_eq!(p3, true);
-}
+        assert!(p1);
+        assert!(p2);
+        assert!(p3);
+    }
 
-#[test]
-fn none_of_for_false() {
-    let vecp = vec![2, 4, 5];
-    let vec = vec![2, 4, 5];
-    let p1 = algo::none_of(&vecp, vecp.after(vecp.start()), vecp.end(), |x| {
-        x % 2 == 1
-    });
-    let p2 = rng::none_of(&vec, |x| x % 2 == 1);
-    let p3 = vec.none_of(|x| x % 2 == 1);
+    #[test]
+    fn none_of_for_false() {
+        let vecp = vec![2, 4, 5];
+        let vec = vec![2, 4, 5];
+        let p1 =
+            algo::none_of(&vecp, vecp.after(vecp.start()), vecp.end(), |x| {
+                x % 2 == 1
+            });
+        let p2 = rng::none_of(&vec, |x| x % 2 == 1);
+        let p3 = vec.none_of(|x| x % 2 == 1);
 
-    assert_eq!(p1, false);
-    assert_eq!(p2, false);
-    assert_eq!(p3, false);
-}
+        assert!(!p1);
+        assert!(!p2);
+        assert!(!p3);
+    }
 
-#[test]
-fn none_of_for_empty() {
-    let vecp = vec![2, 4, 5];
-    let vec = vec![];
-    let p1 = algo::none_of(&vecp, vecp.start(), vecp.start(), |x| x % 2 == 1);
-    let p2 = rng::none_of(&vec, |x| x % 2 == 1);
-    let p3 = vec.none_of(|x| x % 2 == 1);
+    #[test]
+    fn none_of_for_empty() {
+        let vecp = vec![2, 4, 5];
+        let vec = vec![];
+        let p1 =
+            algo::none_of(&vecp, vecp.start(), vecp.start(), |x| x % 2 == 1);
+        let p2 = rng::none_of(&vec, |x| x % 2 == 1);
+        let p3 = vec.none_of(|x| x % 2 == 1);
 
-    assert_eq!(p1, true);
-    assert_eq!(p2, true);
-    assert_eq!(p3, true);
+        assert!(p1);
+        assert!(p2);
+        assert!(p3);
+    }
 }
--- tests/vectest.rs
+++ tests/vectest.rs
@@ -1,53 +1,56 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2024 Rishabh Dwivedi (rishabhdwivedi17@gmail.com)
 
-use stl::*;
+#[cfg(test)]
+pub mod tests {
+    use stl::*;
 
-#[test]
-fn start_position() {
-    let vec = vec![10, 20, 30];
-    assert_eq!(vec.start(), 0)
-}
+    #[test]
+    fn start_position() {
+        let vec = vec![10, 20, 30];
+        assert_eq!(vec.start(), 0)
+    }
 
-#[test]
-fn end_position() {
-    let vec = vec![10, 20, 30];
-    assert_eq!(vec.end(), 3)
-}
+    #[test]
+    fn end_position() {
+        let vec = vec![10, 20, 30];
+        assert_eq!(vec.end(), 3)
+    }
 
-#[test]
-fn position_after() {
-    let vec = vec![10, 20, 30];
-    assert_eq!(vec.after(vec.start()), 1)
-}
+    #[test]
+    fn position_after() {
+        let vec = vec![10, 20, 30];
+        assert_eq!(vec.after(vec.start()), 1)
+    }
 
-#[test]
-fn at() {
-    let vec = vec![10, 20, 30];
-    assert_eq!(*vec.at(&0), 10)
-}
+    #[test]
+    fn at() {
+        let vec = vec![10, 20, 30];
+        assert_eq!(*vec.at(&0), 10)
+    }
 
-#[test]
-fn position_before() {
-    let vec = vec![10, 20, 30];
-    assert_eq!(vec.before(3), 2)
-}
+    #[test]
+    fn position_before() {
+        let vec = vec![10, 20, 30];
+        assert_eq!(vec.before(3), 2)
+    }
 
-#[test]
-fn nth_position_after() {
-    let vec = vec![10, 20, 30];
-    assert_eq!(vec.after_n(0, 2), 2)
-}
+    #[test]
+    fn nth_position_after() {
+        let vec = vec![10, 20, 30];
+        assert_eq!(vec.after_n(0, 2), 2)
+    }
 
-#[test]
-fn nth_position_before() {
-    let vec = vec![10, 20, 30];
-    assert_eq!(vec.before_n(2, 2), 0)
-}
+    #[test]
+    fn nth_position_before() {
+        let vec = vec![10, 20, 30];
+        assert_eq!(vec.before_n(2, 2), 0)
+    }
 
-#[test]
-fn at_mut() {
-    let mut vec = vec![10, 20, 30];
-    *vec.at_mut(&0) = 2;
-    assert_eq!(*vec.at(&0), 2)
+    #[test]
+    fn at_mut() {
+        let mut vec = vec![10, 20, 30];
+        *vec.at_mut(&0) = 2;
+        assert_eq!(*vec.at(&0), 2)
+    }
 }
