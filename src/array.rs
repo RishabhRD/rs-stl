6b52a3a44b43781b9bc8b3c1d94f3b0c4916b0bb
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-12-09 16:15:50 +0530

    implement ranges array

---
+++ src/array.rs
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2024 Rishabh Dwivedi (rishabhdwivedi17@gmail.com)
+
+use crate::{
+    BidirectionalRange, ForwardRange, InputRange, OutputRange,
+    RandomAccessRange,
+};
+
+impl<T, const N: usize> InputRange for [T; N] {
+    type Element = T;
+
+    type Position = usize;
+
+    fn start(&self) -> Self::Position {
+        0
+    }
+
+    fn end(&self) -> Self::Position {
+        self.len()
+    }
+
+    fn after(&self, i: Self::Position) -> Self::Position {
+        i + 1
+    }
+
+    fn at(&self, i: &Self::Position) -> &Self::Element {
+        &self[*i]
+    }
+}
+
+impl<T, const N: usize> ForwardRange for [T; N] {}
+
+impl<T, const N: usize> BidirectionalRange for [T; N] {
+    fn before(&self, i: Self::Position) -> Self::Position {
+        i - 1
+    }
+}
+
+impl<T, const N: usize> RandomAccessRange for [T; N] {
+    fn after_n(&self, i: Self::Position, n: usize) -> Self::Position {
+        i + n
+    }
+
+    fn before_n(&self, i: Self::Position, n: usize) -> Self::Position {
+        i - n
+    }
+}
+
+impl<T, const N: usize> OutputRange for [T; N] {
+    fn at_mut(&mut self, i: &Self::Position) -> &mut Self::Element {
+        &mut self[*i]
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 // Copyright (c) 2024 Rishabh Dwivedi (rishabhdwivedi17@gmail.com)
 
 pub mod algo;
+pub mod array;
 pub mod ranges;
 pub mod regular;
 pub mod rng;
