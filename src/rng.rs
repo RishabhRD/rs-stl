8a603c7adc667f8fda27a52f29b7748c08ac7dff
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-12-18 20:44:23 +0530

    doc: rng module top

--- src/rng.rs
+++ src/rng.rs
@@ -1,48 +1,104 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2024 Rishabh Dwivedi (rishabhdwivedi17@gmail.com)
 
+//! # Ranges module
+//!
+//! The `rng` module provides a collection of range version of STL algorithms
+//! as well as some infix version for some subset of algorithms.
+//!
+//! If infix version of range algorithm is supported, then it is in following
+//! format:
+//! ```rust
+//! use stl::*;
+//! use rng::infix::*;
+//!
+//! let arr = [1, 2, 3];
+//!
+//! let cnt = rng::count_if(&arr, |x| x % 2 == 1);
+//! assert_eq!(cnt, 2);
+//! let cnt = arr.count_if(|x| x % 2 == 1);
+//! assert_eq!(cnt, 2);
+//! ```
+//!
+//! For all algorithms it is mentioned in doc if infix version is supported or
+//! not for that algorithm.
+//!
+//! For using infix version of supported algorithms use:
+//! ```rust
+//! use stl::rng::infix::*;
+//! ```
+
+#[doc(hidden)]
 pub mod find;
+#[doc(inline)]
 pub use find::*;
 
+#[doc(hidden)]
 pub mod count;
+#[doc(inline)]
 pub use count::*;
 
+#[doc(hidden)]
 pub mod of;
+#[doc(inline)]
 pub use of::*;
 
+#[doc(hidden)]
 pub mod mismatch;
+#[doc(inline)]
 pub use mismatch::*;
 
+#[doc(hidden)]
 pub mod adjacent_find;
+#[doc(inline)]
 pub use adjacent_find::*;
 
+#[doc(hidden)]
 pub mod equals;
+#[doc(inline)]
 pub use equals::*;
 
+#[doc(hidden)]
 pub mod copy;
+#[doc(inline)]
 pub use copy::*;
 
+#[doc(hidden)]
 pub mod transform;
+#[doc(inline)]
 pub use transform::*;
 
+#[doc(hidden)]
 pub mod replace;
+#[doc(inline)]
 pub use replace::*;
 
+#[doc(hidden)]
 pub mod fill;
+#[doc(inline)]
 pub use fill::*;
 
+#[doc(hidden)]
 pub mod remove;
+#[doc(inline)]
 pub use remove::*;
 
+#[doc(hidden)]
 pub mod unique;
+#[doc(inline)]
 pub use unique::*;
 
+#[doc(hidden)]
 pub mod reverse;
+#[doc(inline)]
 pub use reverse::*;
 
+#[doc(hidden)]
 pub mod rotate;
+#[doc(inline)]
 pub use rotate::*;
 
+#[doc(hidden)]
 pub mod infix {
     pub use super::adjacent_find::infix::*;
     pub use super::count::infix::*;
