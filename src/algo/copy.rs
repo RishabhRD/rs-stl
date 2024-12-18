e2924a24705c04d4c74f7d393e48ead78ca49d91
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-12-18 20:45:03 +0530

    doc: for copy algorithm

--- src/algo/copy.rs
+++ src/algo/copy.rs
@@ -15,6 +15,20 @@ use crate::{InputRange, OutputRange};
 ///   - Returns the position of dest just after last copy position.
 ///   - Complexity: O(n). Total n pred applications and maximum n copy operations.
 ///     where n is number of elements in `[start, end)` of src.
+///
+/// # Example
+/// ```rust
+/// use stl::*;
+/// use rng::infix::*;
+///
+/// let src = [1, 2, 3];
+/// let mut dest = [0, 0];
+///
+/// let out = dest.start();
+/// let i = algo::copy_if(&src, src.start(), src.end(), &mut dest, out, |x| x % 2 == 1);
+/// assert!(dest.equals(&[1, 3]));
+/// assert_eq!(i, 2);
+/// ```
 pub fn copy_if<SrcRange, DestRange, Pred>(
     src: &SrcRange,
     mut start: SrcRange::Position,
@@ -50,6 +64,21 @@ where
 ///   - copies elements from `[start, end)` to out position of dest.
 ///   - Returns the position of dest just after last copy position.
 ///   - Complexity: O(n). Total n copy operations.
+///
+/// where n is number of elements in `[start, end)`.
+/// # Example
+/// ```rust
+/// use stl::*;
+/// use rng::infix::*;
+///
+/// let src = [1, 2, 3];
+/// let mut dest = [0, 0, 0];
+///
+/// let out = dest.start();
+/// let i = algo::copy(&src, src.start(), src.end(), &mut dest, out);
+/// assert!(dest.equals(&[1, 2, 3]));
+/// assert_eq!(i, 3);
+/// ```
 pub fn copy<SrcRange, DestRange>(
     src: &SrcRange,
     mut start: SrcRange::Position,
@@ -81,6 +110,20 @@ where
 ///   - copies elements from [start, start + n) to out position of dest.
 ///   - Returns the position of dest just after last copy position.
 ///   - Complexity: O(n). Total n copy operations.
+///
+/// # Example
+/// ```rust
+/// use stl::*;
+/// use rng::infix::*;
+///
+/// let src = [1, 2, 3];
+/// let mut dest = [0, 0, 0];
+///
+/// let out = dest.start();
+/// let i = algo::copy_n(&src, src.start(), 2, &mut dest, out);
+/// assert!(dest.equals(&[1, 2, 0]));
+/// assert_eq!(i, 2);
+/// ```
 pub fn copy_n<SrcRange, DestRange>(
     src: &SrcRange,
     mut start: SrcRange::Position,
--- src/rng/copy.rs
+++ src/rng/copy.rs
@@ -3,16 +3,71 @@
 
 use crate::{algo, InputRange, OutputRange};
 
-pub fn copy_if<R, D, F>(rng: &R, dest: &mut D, pred: F) -> D::Position
+/// Copies elements from src to out of dest if it satisfies predicate.
+///
+/// # Precondition
+///   - dest should be able to accomodate all copied elements starting from out.
+///
+/// # Postcondition
+///   - copies elements from src satisfying `pred` to out position of
+///     dest.
+///   - Returns the position of dest just after last copy position.
+///   - Complexity: O(n). Total n pred applications and maximum n copy operations.
+///     where n is number of elements in `[start, end)` of src.
+///
+/// #### Infix Supported
+/// NO
+///
+/// # Example
+/// ```rust
+/// use stl::*;
+/// use rng::infix::*;
+///
+/// let src = [1, 2, 3];
+///
+/// let mut dest = [0, 0];
+/// let i = rng::copy_if(&src, &mut dest, |x| x % 2 == 1);
+/// assert!(dest.equals(&[1, 3]));
+/// assert_eq!(i, 2);
+/// ```
+pub fn copy_if<SrcRange, DestRange, Pred>(
+    src: &SrcRange,
+    dest: &mut DestRange,
+    pred: Pred,
+) -> DestRange::Position
 where
-    R: InputRange<Element = D::Element> + ?Sized,
-    R::Element: Clone,
-    D: OutputRange + ?Sized,
-    F: Fn(&R::Element) -> bool,
+    SrcRange: InputRange<Element = DestRange::Element> + ?Sized,
+    SrcRange::Element: Clone,
+    DestRange: OutputRange + ?Sized,
+    Pred: Fn(&SrcRange::Element) -> bool,
 {
-    algo::copy_if(rng, rng.start(), rng.end(), dest, dest.start(), pred)
+    algo::copy_if(src, src.start(), src.end(), dest, dest.start(), pred)
 }
 
+/// Copies elements from src to out of dest.
+///
+/// # Precondition
+///   - dest should be able to accomodate n elements
+///
+/// # Postcondition
+///   - copies elements from `[start, end)` to out position of dest.
+///   - Returns the position of dest just after last copy position.
+///   - Complexity: O(n). Total n copy operations.
+///
+/// where n is number of elements in src.
+///
+/// # Example
+/// ```rust
+/// use stl::*;
+/// use rng::infix::*;
+///
+/// let src = [1, 2, 3];
+/// let mut dest = [0, 0, 0];
+///
+/// let i = rng::copy(&src, &mut dest);
+/// assert!(dest.equals(&[1, 2, 3]));
+/// assert_eq!(i, 3);
+/// ```
 pub fn copy<R, D>(rng: &R, dest: &mut D) -> D::Position
 where
     R: InputRange<Element = D::Element> + ?Sized,
