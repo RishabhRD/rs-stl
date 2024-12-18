1b9970b768d9e108af78f67d635bb033fe6f6c79
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-12-19 03:34:50 +0530

    doc: remove algorithms

--- src/algo/remove.rs
+++ src/algo/remove.rs
@@ -17,6 +17,19 @@ use crate::{InputRange, OutputRange, Regular};
 ///   - Complexity: O(n). Exactly n applications of pred.
 ///
 ///   Where n is number of elements in `[start, end)`.
+///
+/// # Example
+/// ```rust
+/// use stl::*;
+/// use rng::infix::*;
+///
+/// let mut arr = [1, 2, 3, 4];
+/// let start = arr.start();
+/// let end = arr.end();
+/// let i = algo::remove_if(&mut arr, start, end, |x| x % 2 == 1);
+/// assert_eq!(i, 2);
+/// assert!(&arr[0..i].equals(&[2, 4]));
+/// ```
 pub fn remove_if<Range, Pred>(
     rng: &mut Range,
     mut start: Range::Position,
@@ -61,6 +74,19 @@ where
 ///   - Complexity: O(n). Exactly n equality comparisions.
 ///
 ///   Where n is number of elements in `[start, end)`.
+///
+/// # Example
+/// ```rust
+/// use stl::*;
+/// use rng::infix::*;
+///
+/// let mut arr = [1, 2, 1, 4];
+/// let start = arr.start();
+/// let end = arr.end();
+/// let i = algo::remove(&mut arr, start, end, &1);
+/// assert_eq!(i, 2);
+/// assert!(&arr[0..i].equals(&[2, 4]));
+/// ```
 pub fn remove<Range>(
     rng: &mut Range,
     start: Range::Position,
@@ -77,16 +103,30 @@ where
 /// Copies elements from src to dest omitting elements satisfying given predicate.
 ///
 /// # Precondition
-///   - `[start, end)` represents valid positions in rng
+///   - `[start, end)` represents valid positions in src
 ///   - dest can accomodate copied elements starting from out.
 ///
 /// # Postcondition
-///   - Copies elements from `[start, end)` of `rng` to starting from `out`
+///   - Copies elements from `[start, end)` of `src` to starting from `out`
 ///     of `dest`, omitting the elements which satisfies pred.
 ///   - Returns position of past the last element copied in dest.
 ///   - Complexity: O(n). Exactly n applications of pred.
 ///
 ///   Where n is number of elements in `[start, end)`.
+///
+/// # Example
+/// ```rust
+/// use stl::*;
+/// use rng::infix::*;
+///
+/// let src = [1, 2, 3, 4];
+///
+/// let mut dest = [0, 0];
+/// let out = dest.start();
+/// let i = algo::remove_copy_if(&src, src.start(), src.end(), &mut dest, out, |x| x % 2 == 1);
+/// assert_eq!(i, 2);
+/// assert!(dest.equals(&[2, 4]));
+/// ```
 pub fn remove_copy_if<SrcRange, DestRange, Pred>(
     src: &SrcRange,
     mut start: SrcRange::Position,
@@ -114,16 +154,30 @@ where
 /// Copies elements from src to dest omitting elements equals given element.
 ///
 /// # Precondition
-///   - `[start, end)` represents valid positions in rng
+///   - `[start, end)` represents valid positions in src
 ///   - dest can accomodate copied elements starting from out.
 ///
 /// # Postcondition
-///   - Copies elements from `[start, end)` of `rng` to starting from `out`
+///   - Copies elements from `[start, end)` of `src` to starting from `out`
 ///     of `dest`, omitting the elements == val.
 ///   - Returns position of past the last element copied in dest.
 ///   - Complexity: O(n). Exactly n equality comparisions.
 ///
 ///   Where n is number of elements in `[start, end)`.
+///
+/// # Example
+/// ```rust
+/// use stl::*;
+/// use rng::infix::*;
+///
+/// let src = [1, 2, 1, 4];
+///
+/// let mut dest = [0, 0];
+/// let out = dest.start();
+/// let i = algo::remove_copy(&src, src.start(), src.end(), &mut dest, out, &1);
+/// assert_eq!(i, 2);
+/// assert!(dest.equals(&[2, 4]));
+/// ```
 pub fn remove_copy<SrcRange, DestRange>(
     src: &SrcRange,
     start: SrcRange::Position,
--- src/rng/remove.rs
+++ src/rng/remove.rs
@@ -3,32 +3,155 @@
 
 use crate::{algo, InputRange, OutputRange, Regular};
 
-pub fn remove_if<R, F>(rng: &mut R, pred: F) -> R::Position
+/// Moves all element satisfying predicate to end of range.
+///
+/// # Precondition
+///
+/// # Postcondition
+///   - Removes element satisfying pred by moving them to end of rng.
+///   - NOTE: remove_if doesn't resize rng.
+///   - Relative ordering of elements NOT satisfying pred is preserved.
+///   - Relative ordering of removed elements is NOT guaranteed.
+///   - Returns end position for preserved elements in rng.
+///   - Complexity: O(n). Exactly n applications of pred.
+///
+///   Where n is number of elements in rng.
+///
+/// #### Infix Supported
+/// YES
+///
+/// # Example
+/// ```rust
+/// use stl::*;
+/// use rng::infix::*;
+///
+/// let mut arr = [1, 2, 3, 4];
+/// let i = rng::remove_if(&mut arr, |x| x % 2 == 1);
+/// assert_eq!(i, 2);
+/// assert!(&arr[0..i].equals(&[2, 4]));
+///
+/// let mut arr = [1, 2, 3, 4];
+/// let i = arr.remove_if(|x| x % 2 == 1);
+/// assert_eq!(i, 2);
+/// assert!(&arr[0..i].equals(&[2, 4]));
+/// ```
+pub fn remove_if<Range, Pred>(rng: &mut Range, pred: Pred) -> Range::Position
 where
-    R: OutputRange + ?Sized,
-    F: Fn(&R::Element) -> bool,
+    Range: OutputRange + ?Sized,
+    Pred: Fn(&Range::Element) -> bool,
 {
     algo::remove_if(rng, rng.start(), rng.end(), pred)
 }
 
-pub fn remove<R>(rng: &mut R, val: &R::Element) -> R::Position
+/// Moves all element equals given element to end of range.
+///
+/// # Precondition
+///
+/// # Postcondition
+///   - Removes element == val by moving them to end of rng.
+///   - NOTE: remove doesn't resize rng.
+///   - Relative ordering of preserved elements are maintained.
+///   - Relative ordering of removed elements is NOT guaranteed.
+///   - Returns end position for preserved elements in rng.
+///   - Complexity: O(n). Exactly n equality comparisions.
+///
+///   Where n is number of elements in rng.
+///
+/// #### Infix Supported
+/// YES
+///
+/// # Example
+/// ```rust
+/// use stl::*;
+/// use rng::infix::*;
+///
+/// let mut arr = [1, 2, 1, 4];
+/// let i = rng::remove(&mut arr, &1);
+/// assert_eq!(i, 2);
+/// assert!(&arr[0..i].equals(&[2, 4]));
+///
+/// let mut arr = [1, 2, 1, 4];
+/// let i = arr.remove(&1);
+/// assert_eq!(i, 2);
+/// assert!(&arr[0..i].equals(&[2, 4]));
+/// ```
+pub fn remove<Range>(rng: &mut Range, val: &Range::Element) -> Range::Position
 where
-    R: OutputRange + ?Sized,
-    R::Element: Eq,
+    Range: OutputRange + ?Sized,
+    Range::Element: Eq,
 {
     algo::remove(rng, rng.start(), rng.end(), val)
 }
 
-pub fn remove_copy_if<R, D, F>(rng: &R, dest: &mut D, pred: F) -> D::Position
+/// Copies elements from src to dest omitting elements satisfying given predicate.
+///
+/// # Precondition
+///   - dest can accomodate copied elements.
+///
+/// # Postcondition
+///   - Copies elements from `src` to `dest`, omitting the elements which
+///     satisfies pred.
+///   - Returns position of past the last element copied in dest.
+///   - Complexity: O(n). Exactly n applications of pred.
+///
+///   Where n is number of elements in src.
+///
+/// #### Infix Supported
+/// NO
+///
+/// # Example
+/// ```rust
+/// use stl::*;
+/// use rng::infix::*;
+///
+/// let src = [1, 2, 3, 4];
+///
+/// let mut dest = [0, 0];
+/// let i = rng::remove_copy_if(&src, &mut dest, |x| x % 2 == 1);
+/// assert_eq!(i, 2);
+/// assert!(dest.equals(&[2, 4]));
+/// ```
+pub fn remove_copy_if<SrcRange, DestRange, Pred>(
+    src: &SrcRange,
+    dest: &mut DestRange,
+    pred: Pred,
+) -> DestRange::Position
 where
-    R: InputRange + ?Sized,
-    R::Element: Clone,
-    D: OutputRange<Element = R::Element> + ?Sized,
-    F: Fn(&R::Element) -> bool,
+    SrcRange: InputRange + ?Sized,
+    SrcRange::Element: Clone,
+    DestRange: OutputRange<Element = SrcRange::Element> + ?Sized,
+    Pred: Fn(&SrcRange::Element) -> bool,
 {
-    algo::remove_copy_if(rng, rng.start(), rng.end(), dest, dest.start(), pred)
+    algo::remove_copy_if(src, src.start(), src.end(), dest, dest.start(), pred)
 }
 
+/// Copies elements from src to dest omitting elements equals given element.
+///
+/// # Precondition
+///   - dest can accomodate copied elements.
+///
+/// # Postcondition
+///   - Copies elements from `src` to `dest`, omitting the elements == val.
+///   - Returns position of past the last element copied in dest.
+///   - Complexity: O(n). Exactly n equality comparisions.
+///
+///   Where n is number of elements in src.
+///
+/// #### Infix Supported
+/// NO
+///
+/// # Example
+/// ```rust
+/// use stl::*;
+/// use rng::infix::*;
+///
+/// let src = [1, 2, 1, 4];
+///
+/// let mut dest = [0, 0];
+/// let i = rng::remove_copy(&src, &mut dest, &1);
+/// assert_eq!(i, 2);
+/// assert!(dest.equals(&[2, 4]));
+/// ```
 pub fn remove_copy<R, D>(rng: &R, dest: &mut D, val: &R::Element) -> D::Position
 where
     R: InputRange + ?Sized,
