719dc4c39f924b48a9d6250543da1b5c8ac5f5bb
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-12-18 14:41:18 +0530

    fix more comments

--- src/ranges.rs
+++ src/ranges.rs
@@ -19,12 +19,12 @@ pub trait InputRange {
     fn end(&self) -> Self::Position;
 
     /// Returns position immediately after i
-    //
+    ///
     /// Requires: is_last_position(i) == false
     fn after(&self, i: Self::Position) -> Self::Position;
 
     /// Access element at position i
-    //
+    ///
     /// Requires: i is a valid position in self and is_last_position(i) == false
     fn at(&self, i: &Self::Position) -> &Self::Element;
 }
@@ -33,7 +33,7 @@ pub trait ForwardRange: InputRange<Position: Regular> {}
 
 pub trait BidirectionalRange: ForwardRange {
     /// Returns position immediately before i
-    //
+    ///
     /// Requires: i != first_position()
     fn before(&self, i: Self::Position) -> Self::Position;
 }
@@ -42,24 +42,24 @@ pub trait RandomAccessRange:
     BidirectionalRange<Position: Regular + Ord>
 {
     /// Returns nth position after i
-    //
+    ///
     /// Requires: there should be n valid positions after i
     fn after_n(&self, i: Self::Position, n: usize) -> Self::Position;
 
     /// Returns nth position before i
-    //
+    ///
     /// Requires: there should be n valid positions before i
     fn before_n(&self, i: Self::Position, n: usize) -> Self::Position;
 }
 
 pub trait OutputRange: ForwardRange {
     /// Access element at position i
-    //
+    ///
     /// Requires: i is a valid position in self and is_last_position(i) == false
     fn at_mut(&mut self, i: &Self::Position) -> &mut Self::Element;
 
     /// Swap elements at position i and j
-    //
+    ///
     /// Requires: i and j is a valid position in self.
     fn swap_at(&mut self, i: &Self::Position, j: &Self::Position);
 }
