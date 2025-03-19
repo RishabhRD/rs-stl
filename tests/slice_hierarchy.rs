84e926a8d7e0252c4949d7b8a7e8efc5fe51d2c6
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2025-03-19 23:54:47 +0530

    feat: tests for slice hierarchy

---
+++ tests/slice_hierarchy.rs
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 Rishabh Dwivedi (rishabhdwivedi17@gmail.com)
+
+#[cfg(test)]
+pub mod tests {
+    use details::Basic;
+    use stl::*;
+
+    mod details {
+        use stl::{Collection, ReorderableCollection, Slice, SliceMut};
+
+        #[derive(Clone, PartialEq, Eq)]
+        pub struct Basic {
+            pub data: [i32; 5],
+        }
+
+        impl Collection for Basic {
+            type Position = usize;
+
+            type Element = i32;
+
+            type Whole = Self;
+
+            fn start(&self) -> Self::Position {
+                0
+            }
+
+            fn end(&self) -> Self::Position {
+                5
+            }
+
+            fn after(&self, i: Self::Position) -> Self::Position {
+                i + 1
+            }
+
+            fn at(&self, i: &Self::Position) -> &Self::Element {
+                &self.data[*i]
+            }
+
+            fn slice(
+                &self,
+                from: Self::Position,
+                to: Self::Position,
+            ) -> stl::Slice<Self::Whole> {
+                Slice::new(self, from, to)
+            }
+        }
+
+        impl ReorderableCollection for Basic {
+            fn swap_at(&mut self, i: &Self::Position, j: &Self::Position) {
+                self.data.swap(*i, *j)
+            }
+
+            fn slice_mut(
+                &mut self,
+                from: Self::Position,
+                to: Self::Position,
+            ) -> stl::SliceMut<Self::Whole> {
+                SliceMut::new(self, from, to)
+            }
+        }
+    }
+
+    #[test]
+    fn type_check() {
+        let arr = Basic {
+            data: [1, 2, 3, 4, 5],
+        };
+        let slice: Slice<Basic> = arr.all();
+        assert!(slice == Slice::<Basic>::new(&arr, arr.start(), arr.end()));
+    }
+
+    #[test]
+    fn should_not_nest() {
+        let arr = Basic {
+            data: [1, 2, 3, 4, 5],
+        };
+        let slice1: Slice<Basic> = arr.all();
+        let slice2: Slice<Basic> = slice1.all();
+        assert!(slice1 == slice2);
+    }
+
+    #[test]
+    fn type_check_mut() {
+        let mut arr = Basic {
+            data: [1, 2, 3, 4, 5],
+        };
+        let _: SliceMut<Basic> = arr.all_mut();
+        // Can't assert equality as 2 mutable references are not possible at a time.
+    }
+
+    #[test]
+    fn should_not_nest_mut() {
+        let mut arr = Basic {
+            data: [1, 2, 3, 4, 5],
+        };
+        let mut slice1: SliceMut<Basic> = arr.all_mut();
+        let _: SliceMut<Basic> = slice1.all_mut();
+        // Can't assert equality as 2 mutable references are not possible at a time.
+    }
+}
