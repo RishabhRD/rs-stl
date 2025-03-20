fcc212256d4a55b8b4deeb58cdd08a77ff6bc7ca
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2025-03-21 00:17:59 +0530

    added test for find_if

---
+++ tests/find_test.rs
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 Rishabh Dwivedi (rishabhdwivedi17@gmail.com)
+
+#[cfg(test)]
+pub mod tests {
+    use stl::*;
+
+    #[test]
+    fn find_if_element_found() {
+        let arr = [1, 2, 3, 4];
+        let i = arr.find_if(|x| *x == 2);
+        assert_eq!(i, 1);
+    }
+
+    #[test]
+    fn find_if_element_not_found() {
+        let arr = [1, 3, 4];
+        let i = arr.find_if(|x| *x == 2);
+        assert_eq!(i, 3);
+    }
+
+    #[test]
+    fn find_if_empty() {
+        let arr: [i32; 0] = [];
+        let i = arr.find_if(|x| *x == 2);
+        assert_eq!(i, 0);
+    }
+}
