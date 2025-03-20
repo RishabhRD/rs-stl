6c7a5fc9edaf0236765c6246d258d762b502b7cf
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2025-03-21 00:32:54 +0530

    added tests for for_each

---
+++ tests/for_each_test.rs
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 Rishabh Dwivedi (rishabhdwivedi17@gmail.com)
+
+#[cfg(test)]
+pub mod tests {
+    use stl::*;
+
+    #[test]
+    fn for_each() {
+        let mut sum = 0;
+        let arr = [1, 2, 3];
+        arr.for_each(|e| sum += e);
+        assert_eq!(sum, 6);
+    }
+
+    #[test]
+    fn for_each_mut() {
+        let mut arr = [1, 2, 3];
+        arr.for_each_mut(|e| *e += 1);
+        assert_eq!(arr, [2, 3, 4]);
+    }
+}
