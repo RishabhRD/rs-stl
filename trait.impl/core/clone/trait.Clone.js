c102579374c49de169139af736e14790ba9ec3f0
RishabhRD <RishabhRD@users.noreply.github.com>
2025-08-02 17:20:35 +0000

    deploy: 393da358c6f9e6a6ab51a978558c418ce76e36e4

---
+++ trait.impl/core/clone/trait.Clone.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["stl",[["impl&lt;'a, Whole&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"stl/struct.Slice.html\" title=\"struct stl::Slice\">Slice</a>&lt;'a, Whole&gt;<div class=\"where\">where\n    Whole: <a class=\"trait\" href=\"stl/trait.Collection.html\" title=\"trait stl::Collection\">Collection</a>&lt;Whole = Whole&gt; + <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a>,\n    Whole::<a class=\"associatedtype\" href=\"stl/trait.Collection.html#associatedtype.Position\" title=\"type stl::Collection::Position\">Position</a>: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a>,</div>"],["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"stl/value_ref/struct.ValueRef.html\" title=\"struct stl::value_ref::ValueRef\">ValueRef</a>&lt;T&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1290]}
---
+++ trait.impl/core/default/trait.Default.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["stl",[["impl&lt;E&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"stl/collections/empty/struct.Iter.html\" title=\"struct stl::collections::empty::Iter\">Iter</a>&lt;'_, E&gt;"],["impl&lt;E&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"stl/collections/empty/struct.IterMut.html\" title=\"struct stl::collections::empty::IterMut\">IterMut</a>&lt;'_, E&gt;"],["impl&lt;E&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"stl/collections/empty/struct.LazyIter.html\" title=\"struct stl::collections::empty::LazyIter\">LazyIter</a>&lt;E&gt;"],["impl&lt;E&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"stl/collections/struct.EmptyCollection.html\" title=\"struct stl::collections::EmptyCollection\">EmptyCollection</a>&lt;E&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1254]}
