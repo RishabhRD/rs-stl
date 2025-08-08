45eab98703dabc03b00e649d7e6b9fe1f4912756
RishabhRD <RishabhRD@users.noreply.github.com>
2025-08-08 19:38:24 +0000

    deploy: 527fbff73f510fc9ac95583c06f8f9fe0e40f1f4

---
+++ trait.impl/core/marker/trait.StructuralPartialEq.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["stl",[["impl&lt;'a, Whole&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"struct\" href=\"stl/struct.Slice.html\" title=\"struct stl::Slice\">Slice</a>&lt;'a, Whole&gt;<div class=\"where\">where\n    Whole: <a class=\"trait\" href=\"stl/trait.Collection.html\" title=\"trait stl::Collection\">Collection</a>&lt;Whole = Whole&gt;,</div>"],["impl&lt;'a, Whole&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"struct\" href=\"stl/struct.SliceMut.html\" title=\"struct stl::SliceMut\">SliceMut</a>&lt;'a, Whole&gt;<div class=\"where\">where\n    Whole: <a class=\"trait\" href=\"stl/trait.ReorderableCollection.html\" title=\"trait stl::ReorderableCollection\">ReorderableCollection</a>&lt;Whole = Whole&gt;,</div>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1013]}
