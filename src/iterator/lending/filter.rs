358a988aa3a1c93e0c01e4d4a8f085b8bf6e6091
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2025-09-07 03:04:49 +0530

    added filter lending iterator

---
+++ src/iterator/lending/filter.rs
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 Rishabh Dwivedi (rishabhdwivedi17@gmail.com)
+
+use crate::LendingIterator;
+
+/// Adaptor for base lending iterator that yields only the items which satisfy predicate.
+pub struct FilterIter<I, F>
+where
+    I: LendingIterator,
+    F: FnMut(&I::Item<'_>) -> bool,
+{
+    pub base: I,
+    pub predicate: F,
+}
+
+impl<I, F> FilterIter<I, F>
+where
+    I: LendingIterator,
+    F: FnMut(&I::Item<'_>) -> bool,
+{
+    /// Creates a new instance of filter adaptor.
+    pub fn new(base: I, predicate: F) -> Self {
+        FilterIter { base, predicate }
+    }
+}
+
+impl<I, F> LendingIterator for FilterIter<I, F>
+where
+    I: LendingIterator,
+    F: FnMut(&I::Item<'_>) -> bool,
+{
+    type Item<'a>
+        = I::Item<'a>
+    where
+        Self: 'a;
+
+    fn next(&mut self) -> Option<Self::Item<'_>> {
+        while let Some(e) = self.base.next() {
+            if (self.predicate)(&e) {
+                return Some(e);
+            }
+        }
+        None
+    }
+}
