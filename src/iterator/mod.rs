37f2af841090234c4d2abc53c7fafbabef24aad9
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2025-09-07 01:12:02 +0530

    iterator module

---
+++ src/iterator/mod.rs
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 Rishabh Dwivedi (rishabhdwivedi17@gmail.com)
+
+pub mod collection_iterator;
+#[doc(inline)]
+pub use collection_iterator::*;
+
